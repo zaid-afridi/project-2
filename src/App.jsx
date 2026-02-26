a89e409b6ae0d68c2ef2147a03f51a8e7f103b0d
zaid-afridi <zaidafridi6868@gmail.com>
2026-02-26 16:34:23 +0500

    Create the second project in react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-5xl bg-gray-500 text-white'>Afridi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
